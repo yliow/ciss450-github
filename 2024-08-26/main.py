80da3551487315cbe33ec8df26ae577aaf422fc1
Yihsiang Liow <yliow@ccis.edu>
2024-08-29 14:18:15 -0400

    no msg

---
+++ 2024-08-26/main.py
@@ -0,0 +1,20 @@
+print("hello world")
+print('h')
+x = "hello world"
+print(x, type(x), id(x))
+x = 1
+print(x, type(x), id(x))
+
+i = int(input("gimme i: "))
+print(i, type(i))
+j = int(input("gimme j: "))
+print(j, type(j))
+print(i, "+", j, "=", i + j)
+print(i, "-", j, "=", i - j)
+print(i, "*", j, "=", i * j)
+print(i, "/", j, "=", i / j)
+print(i, "//", j, "=", i // j)
+print(i, "%", j, "=", i % j)
+x = True
+y = True
+x = (i == j)
