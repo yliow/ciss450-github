4e06959ce45d1b1ba1339afe481ecdd30af0c397
Yihsiang Liow <yliow@ccis.edu>
2024-09-25 16:51:28 -0400

    no msg

--- 2024-09-25/bt-knights-tour/main.py
+++ 2024-09-25/bt-knights-tour/main.py
@@ -1,7 +1,18 @@
+def pprint(s):
+    def line(n):
+        return n * '+-' + '+'
+    n = len(s)
+    print(line(n))
+    for row in s:
+        print('|' + ('|'.join(row)) + '|')
+        print(line(n))
+        
+    
 def bt_kt(s, r, c):
     pass
 
 if __name__ == '__main__':
     n = int(input("n: "))
     print(n)
-    
+    s = [[' ' for r in range(n)] for c in range(n)]
+    pprint(s)
