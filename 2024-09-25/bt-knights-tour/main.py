e79392de30749111466eb556ff06b80de2033f93
Yihsiang Liow <yliow@ccis.edu>
2024-09-25 17:20:42 -0400

    no msg

--- 2024-09-25/bt-knights-tour/main.py
+++ 2024-09-25/bt-knights-tour/main.py
@@ -6,13 +6,13 @@ def pprint(n, s):
         print('|' + ('|'.join([str(_) for _ in row])) + '|')
         print(line(n))
         
-def bt_kt(n, s, r=None, c=None):
+def bt_kt(n, s, r=None, c=None, taken=0):
     pprint(n, s)
     if r == None:
         for r in range(n):
             for c in range(n):
                 s[r][c] = 1
-                flag = bt_kt(n, s, r, c)
+                flag = bt_kt(n, s, r, c, 1)
                 if flag:
                     return True
                 else:
@@ -20,18 +20,21 @@ def bt_kt(n, s, r=None, c=None):
         print("HAVE TO BACKTRACK!!!")
         return False
     else:
-        for (dr, dc) in [(-1, +2), (-2, +1), (-2, -1), (-1, -2),
-                         (+1, -2), (+2, -1), (+2, +1), (+1, +2)]:
-            r0, c0 = r + dr, c + dc
-            if 0 <= r0 < n and 0 <= c0 < n and s[r0][c0] == ' ':
-                s[r0][c0] = s[r][c] + 1
-                flag = bt_kt(n, s, r0, c0)
-                if flag:
-                    return True
-                else:
-                    s[r0][c0] = ' '
-        print("HAVE TO BACKTRACK!!!")
-        return False
+        if taken == n * n: # board is filled
+            return True
+        else:
+            for (dr, dc) in [(-1, +2), (-2, +1), (-2, -1), (-1, -2),
+                             (+1, -2), (+2, -1), (+2, +1), (+1, +2)]:
+                r0, c0 = r + dr, c + dc
+                if 0 <= r0 < n and 0 <= c0 < n and s[r0][c0] == ' ':
+                    s[r0][c0] = taken + 1
+                    flag = bt_kt(n, s, r0, c0, taken + 1)
+                    if flag:
+                        return True
+                    else:
+                        s[r0][c0] = ' '
+            print("HAVE TO BACKTRACK!!!")
+            return False
                     
 if __name__ == '__main__':
     n = int(input("n: "))
