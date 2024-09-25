73736ec2b3eb1dbeeb3f67a9287f3d178efd1162
Yihsiang Liow <yliow@ccis.edu>
2024-09-25 17:11:55 -0400

    no msg

--- 2024-09-25/bt-knights-tour/main.py
+++ 2024-09-25/bt-knights-tour/main.py
@@ -1,18 +1,46 @@
-def pprint(s):
+def pprint(n, s):
     def line(n):
         return n * '+-' + '+'
-    n = len(s)
     print(line(n))
     for row in s:
         print('|' + ('|'.join(row)) + '|')
         print(line(n))
         
     
-def bt_kt(s, r, c):
-    pass
-
+def bt_kt(n, s, r=None, c=None):
+    pprint(n, s)
+    if r == None:
+        for r in range(n):
+            for c in range(n):
+                s[r][c] = 1
+                flag = bt_kt(n, s, r, c)
+                if flag:
+                    return True
+                else:
+                    s[r][c] = ' '
+        return False
+    else:
+        for (dr, dc) in [(-1, +2), (-2, +1), (-2, -1), (-1, -2),
+                         (+1, -2), (+2, -1), (+2, +1), (+1, +2)]:
+            r0, c0 = r + dr, c + dc
+            if 0 <= r0 < n and 0 <= c0 < n and s[r0][c0] == ' ':
+                s[r0][c0] = s[r][c] + 1
+                flag = bt_kt(n, s, r0, c0)
+                if flag:
+                    return True
+                else:
+                    s[r0][c0] = ' '
+        print("HAVE TO BACKTRACK!!!")
+        return False
+                    
 if __name__ == '__main__':
     n = int(input("n: "))
-    print(n)
     s = [[' ' for r in range(n)] for c in range(n)]
-    pprint(s)
+
+    flag = bt_kt(n, s)
+    if flag:
+        print("DONE!!!")
+        pprint(s)
+    else:
+        print("FAIL!!!")
+        
