ac57d8f9a667464a62a5d4b4d931f4c24d2fbf4b
Yihsiang Liow <yliow@ccis.edu>
2024-10-16 16:50:50 -0400

    no msg

--- 2024-10-16/main.py
+++ 2024-10-16/main.py
@@ -3,5 +3,7 @@ simulation of ab pruning
 """
 
 def successors(s):
-    if s == "s1":
-        return [("a1", "s2"), ("a2", "s3"), ("a3", "s4")]
+    d = {"s1": [("a1", "s2"), ("a2", "s3"), ("a3", "s4")]
+         "s2": [("a4", "s5"), ("a5", "s6") ("a6", "s7")],
+         "s3": [],
+         "s4": []}
