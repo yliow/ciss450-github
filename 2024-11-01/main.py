977696ab97ad4302348a00b24ea11fea7d71795e
Yihsiang Liow <yliow@ccis.edu>
2024-11-01 17:07:39 -0400

    no msg

--- 2024-11-01/main.py
+++ 2024-11-01/main.py
@@ -1,6 +1,6 @@
 import random; random.seed()
 
-n = 10
+n = 5
 
 def empty(n):
     return [[' ' for _ in range(n)] for _ in range(n)]
@@ -13,13 +13,35 @@ def printboard(m):
     for row in m:
         print("|%s|" % '|'.join(row))
         print(l)
-    
+
 def rand_state(n):
     m = empty(n)
     for r in range(n):
         m[r][random.randrange(0, n)] = 'Q'
     return m
 
+def isdiag(p0, p1):
+    r0,c0 = p0
+    r1,c1 = p1
+    dr = r0 - r1
+    dc = c0 - c1
+    return dr == dc or dr == -dc
+
+def h(m):
+    n = len(m)
+    points = []
+    for r,row in enumerate(m):
+        for c, v in enumerate(row):
+            if m[r][c] == 'Q':
+                points.append((r, c))
+    print(points)
+    for i in range(0, n - 1):
+        for j in range(i + 1, n):
+            p0 = points[i]
+            p1 = points[j]
+            print((i, j), p0, p1, isdiag(p0, p1))
+
 if __name__ == '__main__':
     m = rand_state(n)
     printboard(m)
+    h(m)
